677b7c8be036c6ac110631eb9c76a31c6d5ab25c
Pierre Bourgeois <dragonralph@gmail.com>
2016-03-25 09:02:39 +0100

    Correct DB

--- src/main/java/fr/iutinfo/skeleton/api/BDDFactory.java
+++ src/main/java/fr/iutinfo/skeleton/api/BDDFactory.java
@@ -9,19 +9,16 @@ import fr.iutinfo.skeleton.utils.GetDBPropertyValues;
 @Singleton
 public class BDDFactory {
     private static DBI dbi = null;
-    
+
     public static DBI getDbi() {
-    	GetDBPropertyValues db = new GetDBPropertyValues("db.properties");
-        if(dbi == null) {
-           try {
-			Class.forName("com.mysql.jdbc.Driver");
-			dbi = new DBI("jdbc:mysql://" + db.getHost() + "/" + db.getDb() + ", " + db.getUser() + ", " + db.getPassword());			
-		} catch (ClassNotFoundException e) {
-
-
-			e.printStackTrace();
-		}
-           
+        GetDBPropertyValues db = new GetDBPropertyValues("db.properties");
+        if (dbi == null) {
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                dbi = new DBI("jdbc:mysql://" + db.getHost() + "/" + db.getDb(), db.getUser(), db.getPassword());
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
         }
         return dbi;
     }
