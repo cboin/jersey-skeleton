1e88bae746ff02a15a50657091b48f98b8cea4e3
Pierre Bourgeois <dragonralph@gmail.com>
2016-03-25 09:32:45 +0100

    move + edt

--- src/main/webapp/assets/js/controllersLogged.js
+++ src/main/webapp/assets/js/controllersLogged.js
@@ -5,14 +5,44 @@ edtControllers.controller('LoggedController', ['$scope', function ($scope) {
 }]);
 
 
-
 edtControllers.controller('LogController', ['$scope', function ($scope, $token) {
 
 }]);
 
 edtControllers.controller('EDTController', ['$scope', function ($scope, $token) {
 
-}]);
+
+    $('#calendar').fullCalendar({
+        header: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'month,agendaWeek,agendaDay'
+        },
+        // defaultDate: '2015-09-01',
+        scrollTime: '08:00:00', //Determines how far down the scroll pane is initially scrolled down
+        // start: '10:00', // a start time
+        // end: '16:00', // an end time
+        defaultView: 'agendaWeek',
+        editable: false,
+        eventLimit: true, // allow "more" link when too many events
+        lang: 'fr',
+        weekNumbers: true,
+        eventTextColor: "black",
+        eventBorderColor: "black",
+        color: "lightgrey",
+        dayClick: function (date, jsEvent, view) {
+
+            $('#calendar').fullCalendar('gotoDate', date);
+            $('#calendar').fullCalendar('changeView', 'agendaDay');
+
+        },
+        // SlotEventOverlap : "false" ,
+        timeFormat: 'HH:mm'
+    });
+
+
+}])
+;
 
 
 
