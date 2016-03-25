a3bd7bf858af3a57e98f9d2b8df4bcd5e93d2aa5
Clément Boin <clem.boin@gmail.com>
2016-03-25 11:08:21 +0100

    Prof test

--- src/main/java/fr/iutinfo/skeleton/api/BDDFactory.java
+++ src/main/java/fr/iutinfo/skeleton/api/BDDFactory.java
@@ -3,26 +3,38 @@ package fr.iutinfo.skeleton.api;
 import javax.inject.Singleton;
 
 import org.skife.jdbi.v2.DBI;
+import org.sqlite.SQLiteDataSource;
 
 import fr.iutinfo.skeleton.utils.GetDBPropertyValues;
 
 @Singleton
 public class BDDFactory {
-    private static DBI dbi = null;
-    
-    public static DBI getDbi() {
-    	GetDBPropertyValues db = new GetDBPropertyValues("db.properties");
-        if(dbi == null) {
-           try {
-			Class.forName("com.mysql.jdbc.Driver");
-			dbi = new DBI("jdbc:mysql://" + db.getHost() + "/" + db.getDb() + ", " + db.getUser() + ", " + db.getPassword());			
-		} catch (ClassNotFoundException e) {
+	private static DBI dbi = null;
 
+	public static DBI getDbi() {
+		GetDBPropertyValues db = new GetDBPropertyValues("db.properties");
+		if (dbi == null) {
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+				dbi = new DBI("jdbc:mysql://" + db.getHost() + "/" + db.getDb() + ", " + db.getUser() + ", "
+						+ db.getPassword());
+			} catch (ClassNotFoundException e) {
 
-			e.printStackTrace();
+				e.printStackTrace();
+			}
+
 		}
-           
-        }
-        return dbi;
-    }
+		return dbi;
+	}
+
+	public static DBI getDbiForSqlite() {
+		if (dbi == null) {
+			SQLiteDataSource ds = new SQLiteDataSource();
+			ds.setUrl("jdbc:sqlite:" + System.getProperty("java.io.tmpdir") + System.getProperty("file.separator")
+					+ "data.db");
+			dbi = new DBI(ds);
+		}
+		
+		return dbi;
+	}
 }
